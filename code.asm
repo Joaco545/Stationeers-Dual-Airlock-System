aa4f014b91be9d4ba9227ec25e3129a46a5a4aa2
Joaco545 <38332230+Joaco545@users.noreply.github.com>
2024-07-29 17:41:30 -0300

    Update code.asm
    Implemented airlock cycling

--- code.asm
+++ code.asm
@@ -1,4 +1,8 @@
-# Pin aliases
+# Variable definitions
+define int_pres 90      # Pressures in KPa
+define ext_pres 0
+
+# Pin aliases (DO NOT TOUCH, WILL BREAK)
 alias ext_door d0
 alias ext_vent d1
 alias gas_sensor d2
@@ -7,7 +11,7 @@ alias int_vent d4
 alias other d5
 alias self db
 
-# Communication Protcol Definitions
+# Communication Protocol Definitions
 define cms_init 0       # Code initializing
 define cms_cycle 1      # Airlock cycling
 define cms_oti 2        # Open to Interior
@@ -59,6 +63,27 @@ bne r1 r0 cycle         # state, cycle airlock
 j main                  # Finish loop
 
 cycle:
+l r14 self Setting      # Get current state
+s self Setting cms_cycle# Set coms to cycle
+add r14 r14 -2          # Make oti = 0 for select
+# On register X set external if ote, internal if oti
+select r13 r14 0 3      # current door (d0/d3)
+select r12 r14 1 4      # current vent (d1/d4)
+select r11 r14 3 0      # other door (d0/d3)
+select r10 r14 4 1      # other vent (d1/d4)
+select r9 r14 int_pres ext_pres #target pressure
+s dr13 Open 0           # Close current door
+s dr13 Setting 0        # Update door status
+sleep 1                 # Wait for door to close
+push 2                  # Push gas sensor device
+push r12                # Push current vent
+push 0                  # Push void target pressure
+jal wait_until_pres_target  # Vent the airlock
+push 2                  # Push gas sensor device
+push r10                # Push other vent
+push r9                 # Push target pressure------
+s dr11 Open 1           # Open other door
+s dr11 Setting 1        # Update door status 
 j main
   
 check_state: # returns oti | ote
@@ -83,4 +108,22 @@ pop r15
 yield                   # Wait until
 l r1 other Setting      # other also
 brne r1 r15 -2          # sets its state
+j ra                    # Return
+
+wait_until_pres_target: # Returns void
+pop r0                  # Get target Pressure
+pop r1                  # Get vent
+pop r2                  # Get gas sensor
+l r3 dr2 Pressure       # Read current pressure
+beq r0 r3 ra            # Return if on target
+s dr1 PressureExternal r0   # Set pressure target
+brlt r3 r0 3            # Pressure < Target set Outw
+s dr1 Mode 1            # else set Inward
+jr 2
+s dr1 Mode 0            # Set vent Outward
+s dr1 On 1              # Turn on the vent
+yield
+l r3 dr2 Pressure       # Read current pressure
+brne r3 r0 -2           # If not equal, loop back
+s dr1 On 0              # If equal turn off the vent
 j ra                    # Return
